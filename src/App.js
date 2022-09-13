b23463c537c8ac048a580b97d6e5d8bb4acb3942
Vladyslav <65622620+Naykitin@users.noreply.github.com>
2022-09-13 17:11:39 +0300

    add search

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Cart from './components/Cart';
 function App() {
   const [items, setItems] = React.useState([]);
   const [cartItems, setCartItems] = React.useState([]);
+  const [search, setSearch] = React.useState('');
   const [cartOpened, setCartOpened] = React.useState(false);
 
   React.useEffect(() => {
@@ -17,14 +18,11 @@ function App() {
   }, []);
 
   const onAddToCart = (currItem) => {
-    // console.log(cartItems);
-    // console.log(currItem);
-    // console.log(cartItems.includes(currItem));
-    if (cartItems.filter((i) => i.includes(currItem))) {
-      return;
-    } else {
-      setCartItems((prev) => [...prev, currItem])
-    }
+    setCartItems((prev) => [...prev, currItem])
+  }
+
+  const onSearch = (event) => {
+    setSearch(event.target.value);
   }
 
 
@@ -34,16 +32,17 @@ function App() {
       <Header onClickCart = {() => setCartOpened(true)} />
       <div className="content">
         <div className="contentTop">
-          <h1>All sneakers</h1>
+          <h1>{search ? `Search by: ${search}` : "All sneakers" }</h1>
           <div className="search-block">
             <img width={18} height={18} src="/img/search.svg" alt="Search" />
-            <input placeholder="Search..." />
+            <input placeholder="Search..." value={search} onChange={onSearch} />
+            {search && <img onClick={() => setSearch('')} className='clear' width="22" height="22" src="/img/plus.svg" alt="Clear" />}
           </div>
         </div>
         <div className="cards">
           {
-            items.map((sneaker) => (
-              <Card id={sneaker.id} image={sneaker.image} title={sneaker.title} price={sneaker.price} onPlus={(currItem) => onAddToCart(currItem)} />
+            items.filter((item) => item.title.toLowerCase().includes(search.toLowerCase())).map((sneaker) => (
+              <Card key={sneaker.id} image={sneaker.image} title={sneaker.title} price={sneaker.price} onPlus={(currItem) => onAddToCart(currItem)} />
             ))
           }
         </div>
